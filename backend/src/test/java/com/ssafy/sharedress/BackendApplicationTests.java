68c37e88d39a0e04bf8b21c60ef2652985a2c75f
주노리 <777joonho@kookmin.ac.kr>
2025-04-21 17:47:18 +0900

    chore: root project

---
+++ backend/src/test/java/com/ssafy/sharedress/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sharedress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
