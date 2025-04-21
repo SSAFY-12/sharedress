d67f9b81605a6a5b75d357d69fc441cfe7d9b92b
DESKTOP-4QCTMKG\SSAFY <jumin9774@naver.com>
2025-04-21 15:30:19 +0900

    feat: 프론트엔드 프로젝트 세팅(eslint, prettier)

---
+++ FE/src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react';
+import './App.css';
+
+export const App = () => (
+	// const hell = 'hello';
+	<div className='flex h-screen items-center justify-center bg-gray-100'>
+		<h1 className='text-4xl font-bold text-blue-600'>Hello Tailwind</h1>
+	</div>
+);
+
+export default App;
