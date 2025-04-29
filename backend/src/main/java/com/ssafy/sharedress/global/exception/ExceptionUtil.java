51ab44ece0f1a9d204938b05166c2cd3e4ea22b7
주노리 <777joonho@kookmin.ac.kr>
2025-04-29 18:21:01 +0900

    ✨ feat: lambda에서 사용할 수 있는 Exception 메서드 작성

--- backend/src/main/java/com/ssafy/sharedress/global/exception/ExceptionUtil.java
+++ backend/src/main/java/com/ssafy/sharedress/global/exception/ExceptionUtil.java
@@ -1,9 +1,15 @@
 package com.ssafy.sharedress.global.exception;
 
+import java.util.function.Supplier;
+
 import com.ssafy.sharedress.global.response.ResponseCode;
 
 public class ExceptionUtil {
 	public static void throwException(ResponseCode code) {
 		throw new BaseException(code);
+	}
+
+	public static Supplier<BaseException> exceptionSupplier(ResponseCode code) {
+		return () -> new BaseException(code);
 	}
 }
