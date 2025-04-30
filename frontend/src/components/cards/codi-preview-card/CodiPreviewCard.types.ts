1b81b32a814d42261776af43e41428e608845289
DESKTOP-4QCTMKG\SSAFY <jumin9774@naver.com>
2025-04-30 12:17:11 +0900

    ✨ feat: cloth-preview-card 요소 수정

--- frontend/src/components/cards/codi-preview-card/CodiPreviewCard.types.ts
+++ frontend/src/components/cards/codi-preview-card/CodiPreviewCard.types.ts
@@ -5,5 +5,9 @@ export interface ClothItem {
 }
 
 export interface CodiPreviewCardProps {
-	items: ClothItem[];
+	items: ClothItem[]; // 코디에 포함된 옷 리스트
+	userName?: string; // 코디 작성자 이름
+	userAvatar?: string; // 코디 작성자 아바타
+	onClick?: () => void; // 클릭 이벤트
+	className?: string; // 추가 스타일링
 }
