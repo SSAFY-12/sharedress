d11ec4acdad7e3a1bc817f68187c1a49859b59b3
JUMINAHN <jumin9774@naver.com>
2025-05-01 10:17:21 +0900

    ☑️ test: bottomsheet test

--- frontend/src/pages/WardrobePage.tsx
+++ frontend/src/pages/WardrobePage.tsx
@@ -1,7 +1,21 @@
-const WardrobePage = () => (
-	<div>
-		<h1>WardrobePage</h1>
-	</div>
-);
+// import { BottomSheet } from '@/components/modals/bottom-sheet';
+
+const WardrobePage = () => {
+	return (
+		<div>
+			<h1>WardrobePage</h1>
+			{/* bottomsheet test */}
+			{/* <div>
+				<h5>test</h5>
+				<BottomSheet
+					isOpen={true}
+					onClose={() => {}}
+					children={'hello'}
+					className=''
+				/>
+			</div> */}
+		</div>
+	);
+};
 
 export default WardrobePage;
