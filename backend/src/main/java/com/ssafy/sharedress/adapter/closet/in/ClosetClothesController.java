cc58c568be7ed710cca15b5e3a930360b462a0a6
주노리 <777joonho@kookmin.ac.kr>
2025-05-02 02:09:17 +0900

    ✨ feat: 옷장 옷 조회 controller 구현

---
+++ backend/src/main/java/com/ssafy/sharedress/adapter/closet/in/ClosetClothesController.java
@@ -0,0 +1,45 @@
+package com.ssafy.sharedress.adapter.closet.in;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.sharedress.application.closet.dto.ClosetClothesResponse;
+import com.ssafy.sharedress.application.closet.usecase.ClosetClothesUseCase;
+import com.ssafy.sharedress.global.dto.CursorPageResult;
+import com.ssafy.sharedress.global.response.ResponseWrapper;
+import com.ssafy.sharedress.global.response.ResponseWrapperFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ClosetClothesController {
+
+	private final ClosetClothesUseCase closetClothesUseCase;
+
+	@GetMapping("/closet/{memberId}")
+	public ResponseEntity<ResponseWrapper<List<ClosetClothesResponse>>> getMemberClosetClothes(
+		@PathVariable("memberId") Long targetMemberId,
+		@RequestParam(required = false) Long categoryId,
+		@RequestParam(required = false) Long cursor,
+		@RequestParam(defaultValue = "20") int size
+	) {
+		// TODO[준]: security context 에서 myId 가져오기
+		Long myId = 1L;
+		CursorPageResult<ClosetClothesResponse> result = closetClothesUseCase.getMemberClosetClothes(
+			myId,
+			targetMemberId,
+			categoryId,
+			cursor,
+			size
+		);
+
+		return ResponseWrapperFactory.toPageResponseEntity(HttpStatus.OK, result);
+	}
+}
