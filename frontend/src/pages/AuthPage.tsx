3eb860a050076438d716c627869fc485d33bf3f1
JUMINAHN <jumin9774@naver.com>
2025-05-01 11:17:52 +0900

    🎨 style: mobile-web 의류 이미지 삽입

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,6 +1,14 @@
+// Auth 관련 데이터 여기 넣으면 될 것 같음
+
 const AuthPage = () => (
 	<div>
-		<h1>AuthPage</h1>
+		{/* 모바일 레이아웃 */}
+		<div className='block sm:hidden min-h-screen bg-auth-bg'></div>
+
+		{/* 웹 레이아웃 - 모바일 에뮬레이션 */}
+		<div className='hidden sm:flex min-h-screen items-center justify-center bg-neutral-900'>
+			<div className='w-[390px] h-[844px] rounded-xl overflow-hidden shadow-xl bg-auth-bg'></div>
+		</div>
 	</div>
 );
 
