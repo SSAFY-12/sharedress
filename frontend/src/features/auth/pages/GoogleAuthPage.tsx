fb7f2d3857b9d2441720509f984a6d77cb4d0d64
JUMINAHN <jumin9774@naver.com>
2025-05-01 19:43:17 +0900

    ✨ feat: Backend token 전송을 위한 1차 flow 생성

--- frontend/src/features/auth/pages/GoogleAuthPage.tsx
+++ frontend/src/features/auth/pages/GoogleAuthPage.tsx
@@ -1,33 +1,33 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 
 const GoogleAuthPage = () => {
-	const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
-	// 들어오는 데이터 입력값
-	// {
-	//     clientId : ""
-	//     credential: ""
-	//     select_by: ""
-	//     }
-
-	// 커스텀 로그인 버튼 -> 사용자 정의 로그인 버튼
+	const login = useGoogleLogin({
+		onSuccess: (response) => {
+			// 승인 코드 플로우
+			if ('code' in response) {
+				console.log('Authorization Code:', response.code);
+				// TODO: 이 코드를 백엔드로 전송하여 액세스 토큰으로 교환
+			}
+		},
+		onError: () => {
+			console.log('Login Failed');
+		},
+		flow: 'auth-code',
+	});
 
 	return (
-		<GoogleOAuthProvider clientId={`${clientId}`}>
-			{/* 팝업 모드 사용시 빈 창 문제가 발생하지 않도록 Cross Origin Opener Poilcy 설정 */}
-			<GoogleLogin
-				// 성공
-				onSuccess={(credentialResponse) => {
-					console.log(credentialResponse);
-				}}
-				// 실패
-				onError={() => {
-					console.log('Login Failed');
-				}}
+		// btn 커스터마이징
+		<button
+			onClick={() => login()}
+			className='flex items-center gap-2.5 px-10 py-2.5 bg-white border border-gray-200 rounded-md cursor-pointer hover:bg-gray-50 transition-colors duration-200 text-base font-bold shadow-sm'
+		>
+			<img
+				src='https://developers.google.com/identity/images/g-logo.png'
+				alt='Google logo'
+				className='w-[18px] h-[18px]'
 			/>
-			;
-		</GoogleOAuthProvider>
+			구글로 시작하기
+		</button>
 	);
 };
 
