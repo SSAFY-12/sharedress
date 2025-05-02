8f686b8d5d3d65e6041bfc6dbadff0135e6d1b80
지윤 <jiyun0218@naver.com>
2025-05-02 15:00:46 +0900

    ✨ feat: 쇼핑몰 에러 코드 정의

---
+++ backend/src/main/java/com/ssafy/sharedress/domain/shoppingmall/error/ShoppingMallErrorCode.java
@@ -0,0 +1,34 @@
+package com.ssafy.sharedress.domain.shoppingmall.error;
+
+import org.springframework.http.HttpStatus;
+
+import com.ssafy.sharedress.global.response.ResponseCode;
+
+public enum ShoppingMallErrorCode implements ResponseCode {
+	SHOPPING_MALL_NOT_FOUND("404", "쇼핑몰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+	private final String code;
+	private final String message;
+	private final HttpStatus httpStatus;
+
+	ShoppingMallErrorCode(String code, String message, HttpStatus httpStatus) {
+		this.code = code;
+		this.message = message;
+		this.httpStatus = httpStatus;
+	}
+
+	@Override
+	public String getCode() {
+		return this.code;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
+}
