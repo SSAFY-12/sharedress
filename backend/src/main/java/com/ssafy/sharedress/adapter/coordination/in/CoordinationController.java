f6331bfd041d0a430f83ba8f79840b235cc08426
주노리 <777joonho@kookmin.ac.kr>
2025-05-03 21:52:54 +0900

    ✨ feat: 코디 목록 조회 API controller 작성

--- backend/src/main/java/com/ssafy/sharedress/adapter/coordination/in/CoordinationController.java
+++ backend/src/main/java/com/ssafy/sharedress/adapter/coordination/in/CoordinationController.java
@@ -1,14 +1,21 @@
 package com.ssafy.sharedress.adapter.coordination.in;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.sharedress.application.coordination.dto.CoordinationRequest;
 import com.ssafy.sharedress.application.coordination.dto.CoordinationResponse;
+import com.ssafy.sharedress.application.coordination.dto.CoordinationWithItemResponse;
+import com.ssafy.sharedress.application.coordination.dto.Scope;
+import com.ssafy.sharedress.application.coordination.usecase.CoordinationQueryUseCase;
 import com.ssafy.sharedress.application.coordination.usecase.CoordinationUseCase;
 import com.ssafy.sharedress.global.response.ResponseWrapper;
 import com.ssafy.sharedress.global.response.ResponseWrapperFactory;
@@ -20,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class CoordinationController {
 
 	private final CoordinationUseCase coordinationUseCase;
+	private final CoordinationQueryUseCase coordinationQueryUseCase;
 
 	@PostMapping("/coordinations/my")
 	public ResponseEntity<ResponseWrapper<CoordinationResponse>> saveMyCoordination(
@@ -37,6 +45,16 @@ public class CoordinationController {
 	) {
 		Long myId = 1L; // TODO[준]: security context 에서 myId 가져오기
 		CoordinationResponse response = coordinationUseCase.recommendCoordination(myId, memberId, coordinationRequest);
+		return ResponseWrapperFactory.toResponseEntity(HttpStatus.OK, response);
+	}
+
+	@GetMapping("/coordinations")
+	public ResponseEntity<ResponseWrapper<List<CoordinationWithItemResponse>>> getCoordinations(
+		@RequestParam(name = "memberId") Long memberId,
+		@RequestParam(name = "scope", defaultValue = "CREATED") Scope scope
+	) {
+		Long myId = 1L; // TODO[준]: security context 에서 myId 가져오기
+		List<CoordinationWithItemResponse> response = coordinationQueryUseCase.getCoordinations(myId, memberId, scope);
 		return ResponseWrapperFactory.toResponseEntity(HttpStatus.OK, response);
 	}
 }
