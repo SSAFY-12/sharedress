0babe553fa9628ed9d89342f47ca8ca723bc64ae
주노리 <777joonho@kookmin.ac.kr>
2025-05-03 21:52:18 +0900

    ✨ feat: 본인이 만든 본인 코드 조회 로직 작성

---
+++ backend/src/main/java/com/ssafy/sharedress/application/coordination/usecase/CoordinationQueryUseCase.java
@@ -0,0 +1,11 @@
+package com.ssafy.sharedress.application.coordination.usecase;
+
+import java.util.List;
+
+import com.ssafy.sharedress.application.coordination.dto.CoordinationWithItemResponse;
+import com.ssafy.sharedress.application.coordination.dto.Scope;
+
+public interface CoordinationQueryUseCase {
+
+	List<CoordinationWithItemResponse> getCoordinations(Long myId, Long targetMemberId, Scope scope);
+}
