a299e1f6a3c5b52ccd60e8b1b1cf48e2c36a23a7
주노리 <777joonho@kookmin.ac.kr>
2025-05-05 01:21:22 +0900

    ✨ feat: 코디 요청 API 기능 구현

---
+++ backend/src/main/java/com/ssafy/sharedress/application/coordination/service/CoordinationRequestService.java
@@ -0,0 +1,34 @@
+package com.ssafy.sharedress.application.coordination.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.sharedress.application.coordination.dto.CoordinationRequestRequest;
+import com.ssafy.sharedress.application.coordination.usecase.CoordinationRequestUseCase;
+import com.ssafy.sharedress.domain.coordination.repository.CoordinationRequestRepository;
+import com.ssafy.sharedress.domain.member.entity.Member;
+import com.ssafy.sharedress.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CoordinationRequestService implements CoordinationRequestUseCase {
+
+	private final MemberRepository memberRepository;
+	private final CoordinationRequestRepository coordinationRequestRepository;
+
+	@Override
+	public void sendCoordinationRequest(Long myId, CoordinationRequestRequest request) {
+		Member requester = memberRepository.getReferenceById(myId);
+		Member receiver = memberRepository.getReferenceById(request.receiverId());
+
+		coordinationRequestRepository.save(
+			request.toEntity(
+				requester,
+				receiver
+			)
+		);
+
+		// TODO[준]: 알림 전송 로직 추가
+	}
+}
--- backend/src/main/java/com/ssafy/sharedress/domain/notification/entity/NotificationType.java
+++ backend/src/main/java/com/ssafy/sharedress/domain/notification/entity/NotificationType.java
@@ -2,6 +2,6 @@ package com.ssafy.sharedress.domain.notification.entity;
 
 public enum NotificationType {
 	FRIEND,     // 친구 요청
-	COORDI,     // 코디 요청
+	COORDINATION,     // 코디 요청
 	COMMENT     // 코디 댓글
 }
