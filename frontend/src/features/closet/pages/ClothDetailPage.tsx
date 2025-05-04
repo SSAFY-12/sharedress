279f998897bc1cbab5cf0cdd6b9febc609256a08
Yeseung-Park <yspark8538@naver.com>
2025-05-05 01:43:33 +0900

    ✨ feat: 변수 컨벤션 지켜서 변수명 수정

--- frontend/src/features/closet/pages/ClothDetailPage.tsx
+++ frontend/src/features/closet/pages/ClothDetailPage.tsx
@@ -1,11 +1,11 @@
 import { ClothItem } from '@/components/cards/cloth-card';
 import Header from '@/components/layouts/Header';
 import { CodiEditor } from '@/containers/CodiEditor';
-import ClothDetailItem from '../components/ClothDetailItem';
+import ClothDetailItem from '@/features/closet/components/ClothDetailItem';
 import NavBar from '@/components/layouts/NavBar';
 import { useNavigate } from 'react-router-dom';
 
-const clothItem: ClothItem = {
+const SampleClothItem: ClothItem = {
 	id: '1',
 	category: '아우터',
 	name: '빈티지 워싱 카라 투웨이 크롭 데님자켓',
@@ -28,10 +28,13 @@ const ClothDetailPage = () => {
 			<Header showBack={true} onBackClick={handleBackClick} />
 
 			<div className='flex-1 overflow-auto pb-20'>
-				<CodiEditor item={clothItem}>
+				<CodiEditor item={SampleClothItem}>
 					<div className='px-4'>
-						<ClothDetailItem label='상품명' value={clothItem.name} />
-						<ClothDetailItem label='카테고리' value={clothItem.category} />
+						<ClothDetailItem label='상품명' value={SampleClothItem.name} />
+						<ClothDetailItem
+							label='카테고리'
+							value={SampleClothItem.category}
+						/>
 						<ClothDetailItem label='브랜드' value='앤드모어' />
 						<ClothDetailItem label='색깔' value='데님' />
 					</div>
