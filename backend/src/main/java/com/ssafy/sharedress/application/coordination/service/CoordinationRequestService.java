f159a2f4b1e77befe36f3391ebbd27b52b5bb335
주노리 <777joonho@kookmin.ac.kr>
2025-05-05 01:24:05 +0900

    ✨ feat: 코디 요청 시 친구 검증 추가

--- backend/src/main/java/com/ssafy/sharedress/application/coordination/service/CoordinationRequestService.java
+++ backend/src/main/java/com/ssafy/sharedress/application/coordination/service/CoordinationRequestService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.ssafy.sharedress.application.coordination.dto.CoordinationRequestRequest;
 import com.ssafy.sharedress.application.coordination.usecase.CoordinationRequestUseCase;
 import com.ssafy.sharedress.domain.coordination.repository.CoordinationRequestRepository;
+import com.ssafy.sharedress.domain.friend.repository.FriendRepository;
 import com.ssafy.sharedress.domain.member.entity.Member;
 import com.ssafy.sharedress.domain.member.repository.MemberRepository;
 
@@ -15,12 +16,17 @@ import lombok.RequiredArgsConstructor;
 public class CoordinationRequestService implements CoordinationRequestUseCase {
 
 	private final MemberRepository memberRepository;
+	private final FriendRepository friendRepository;
 	private final CoordinationRequestRepository coordinationRequestRepository;
 
 	@Override
 	public void sendCoordinationRequest(Long myId, CoordinationRequestRequest request) {
 		Member requester = memberRepository.getReferenceById(myId);
 		Member receiver = memberRepository.getReferenceById(request.receiverId());
+
+		if (!friendRepository.existsByMemberId(requester.getId(), receiver.getId())) {
+			return;
+		}
 
 		coordinationRequestRepository.save(
 			request.toEntity(
