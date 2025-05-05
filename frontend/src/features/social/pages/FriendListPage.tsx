f76a5b5fcc93ac36d3287e8a4023089804ec9780
JUMINAHN <jumin9774@naver.com>
2025-05-05 17:47:50 +0900

    ✨ feat: 내 친구 리스트에서 친구 검색 로직 구현 추가

--- frontend/src/features/social/pages/FriendListPage.tsx
+++ frontend/src/features/social/pages/FriendListPage.tsx
@@ -2,6 +2,7 @@ import { SearchBar } from '@/components/inputs/search-bar';
 import { UserRowItem } from '@/containers/UserRowItem';
 import { useState } from 'react';
 import useFriendList from '@/features/social/hooks/useFriendList';
+import useSearchFriend from '../hooks/useSearchFriend';
 
 // 더미 데이터
 // const friends = [
@@ -45,11 +46,12 @@ import useFriendList from '@/features/social/hooks/useFriendList';
 
 // 메인 컴포넌트
 export const FriendsListPage = () => {
+	// 따로 필터를 안 걸어도 됨 -> 자체 사용자 정보 리스트에서 완료
 	const [searchValue, setSearchValue] = useState(''); //친구 검색 값 -> 친구 검색 이벤트 전송시 서버 로직 ==> 지속해서 바뀌는 동적인 것
-	// const [submitValue, setSubmitValue] = useState(''); // 검색 이벤트 전송시 서버 로직
-	// const [searchResult, setSearchResult] = useState([]);
+	const [resultValue, setResultValue] = useState(''); // 검색 이벤트 전송시 서버 로직
 	// 내 친구 목록 검색
 	const { data: friends } = useFriendList();
+	const { searchMyFriend } = useSearchFriend(resultValue);
 	// const { data: friends, isLoading, error } = useFriendList();
 	// const { data: searchFriend, isLoading, error } = useSearchFriend(submitValue); // 검색 결과 목록 영역
 	// searchValue가 계속적으로 바뀔텐데 ..??? -> 결과 데이터 반환될 것 근데 이 useSearch의 케이스가 다양하다..
@@ -59,9 +61,11 @@ export const FriendsListPage = () => {
 	// 그럼 검색 로직에서 필터링 하는거임
 
 	const handleSearch = (e: any) => {
-		e.preventDefault(); //이게 지금 필요한걸까?
-		// setSubmitValue(searchValue);
-		// 검색 로직 구현 -> 검색 결과 목록 영역에 데이터 반환
+		if (e.key === 'Enter') {
+			e.preventDefault(); //이게 지금 필요한걸까?
+			setResultValue(searchValue);
+			// 검색 로직 구현 -> 검색 결과 목록 영역에 데이터 반환
+		}
 	};
 
 	return (
@@ -72,7 +76,8 @@ export const FriendsListPage = () => {
 					placeholder='친구 검색'
 					value={searchValue}
 					onChange={(e) => setSearchValue(e.target.value)}
-					onSubmit={handleSearch}
+					onKeyDown={handleSearch}
+					// onSubmit={handleSearch}
 					//onsubmit은 제출,, -> enter 누르면 검색 이벤트 전송하도록
 					// 제출할때, 마지막 공백은 trim으로 처리
 				/>
@@ -100,9 +105,22 @@ export const FriendsListPage = () => {
 					))}
 				</div>
 			) : (
-				// 검색 결과 목록 영역 -> 이게 바로 ..? 제출까지는 아닌거같은데 searchValue로 나오는게 맞나
+				// 검색 결과 목록
 				<div>
-					<p>특정 ID를 검색한 후, enter를 눌러주세요.</p>
+					{Array.isArray(searchMyFriend) && searchMyFriend.length > 0 ? (
+						searchMyFriend.map((friend) => (
+							<UserRowItem
+								key={friend.id}
+								userName={friend.nickname}
+								userAvatar={friend.profileImage}
+								userStatus={friend.oneLiner}
+								actionType='arrow'
+								onClick={() => console.log('Navigate to user profile')}
+							/>
+						))
+					) : (
+						<p>검색 결과가 없습니다.</p>
+					)}
 				</div>
 			)}
 		</div>
