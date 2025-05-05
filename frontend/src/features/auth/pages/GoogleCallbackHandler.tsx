f65072d12d00feb4d3d952f2ca5e0afe1d4dec9c
JUMINAHN <jumin9774@naver.com>
2025-05-05 22:40:56 +0900

    🛠️ fix: mutation으로 인한 의존성 배열 routing 문제 해결

--- frontend/src/features/auth/pages/GoogleCallbackHandler.tsx
+++ frontend/src/features/auth/pages/GoogleCallbackHandler.tsx
@@ -1,26 +1,37 @@
 // src/auth/GoogleCallbackHandler.tsx
 // import { useAuthStore } from '@/store/useAuthStore';
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '@/features/auth/hooks/useAuth';
 
 const GoogleCallbackHandler = () => {
 	const { mutation } = useAuth();
-
-	const handleToken = useCallback(() => {
-		const urlHash = new URLSearchParams(window.location.hash.substring(1));
-		const accessToken = urlHash.get('access_token');
-
-		if (accessToken) {
-			console.log(accessToken, 'test!!!!!!!!');
-			mutation.mutate(accessToken);
-		} else {
-			console.error('No access token found');
-		}
-	}, [mutation]);
+	const navigate = useNavigate();
 
 	useEffect(() => {
+		const handleToken = () => {
+			const urlHash = new URLSearchParams(window.location.hash.substring(1));
+			const accessToken = urlHash.get('access_token');
+
+			if (accessToken) {
+				console.log(accessToken, 'test!!!!!!!!');
+				mutation.mutate(accessToken, {
+					onSuccess: () => {
+						navigate('/wardrobe');
+					},
+					onError: (error) => {
+						console.error('토큰 검증 실패:', error);
+						navigate('/login');
+					},
+				});
+			} else {
+				console.error('토큰을 찾을 수 없습니다.');
+				navigate('/login');
+			}
+		};
+
 		handleToken();
-	}, [handleToken]);
+	}, [mutation, navigate]);
 
 	return <div>구글 로그인 중입니다...</div>;
 };
