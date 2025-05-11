f615d878fd95fc1dfb3648af22bd3566bea4a6da
김현래 <gusto917@naver.com>
2025-05-11 20:23:54 +0900

    feat: 코디만드릭도 라우팅

--- frontend/src/components/modals/sub-btn-modal/SubBtnModal.tsx
+++ frontend/src/components/modals/sub-btn-modal/SubBtnModal.tsx
@@ -72,7 +72,7 @@ export const SubBtnModal = ({
 									onClick={() => {
 										console.log('코디 만들기');
 										onClose();
-										navigate('/regist');
+										navigate('/codi/edit');
 									}}
 								>
 									<img src='/icons/create_codi_add.svg' alt='plus' />
