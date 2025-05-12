6dabca13757d7f2685246b7f8c3aac9052f680e4
Yeseung-Park <yspark8538@naver.com>
2025-05-12 14:20:31 +0900

    ✨ feat: 코디 이미지로 저장하기 드디어 성공!

--- frontend/src/features/codi/api/codiApi.ts
+++ frontend/src/features/codi/api/codiApi.ts
@@ -1,4 +1,7 @@
 import client from '@/api/client';
+import { replaceImageWithBase64 } from '../utils/replaceImageWithBase64';
+import html2canvas from 'html2canvas';
+import { waitAllImagesLoaded } from '../utils/watiForAllImages';
 
 interface Position {
 	x: number;
@@ -36,5 +39,53 @@ export const recommendedCodiSaveApi = async (
 		data,
 	);
 	console.log(response.data.content);
+	return response.data;
+};
+
+export const captureCanvasImage = async (
+	element: HTMLElement,
+): Promise<string> => {
+	console.log('[DEBUG] element.innerHTML:', element.innerHTML);
+	console.log(
+		'[DEBUG] element.clientWidth / clientHeight:',
+		element.clientWidth,
+		element.clientHeight,
+	);
+	await waitAllImagesLoaded(element);
+	await replaceImageWithBase64(element);
+	await new Promise((r) => requestAnimationFrame(r));
+
+	const canvas = await html2canvas(element, {
+		useCORS: true,
+		scale: 2,
+	});
+
+	const base64 = canvas.toDataURL('image/png');
+
+	if (!base64.startsWith('data:image/') || base64.length < 1000) {
+		throw new Error('캔버스 이미지 변환 실패');
+	}
+
+	return base64;
+};
+
+export const uploadCodiThumbnail = async (
+	coordinationId: number,
+	file: File,
+) => {
+	const formData = new FormData();
+	formData.append('thumbnail', file);
+
+	console.log('[DEBUG] FormData에 추가된 파일:', file);
+
+	const response = await client.patch(
+		`api/coordinations/${coordinationId}/thumbnail`,
+		formData,
+		{
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+		},
+	);
 	return response.data.content;
 };
