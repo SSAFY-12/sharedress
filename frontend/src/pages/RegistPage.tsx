f0fe0c83f6e40ddcba66f3288af858605ad77b60
김현래 <gusto917@naver.com>
2025-05-12 14:45:44 +0900

    fix: 사용안하는 test페이지 삭제

--- frontend/src/pages/RegistPage.tsx
+++ frontend/src/pages/RegistPage.tsx
@@ -3,14 +3,12 @@ import RegistScanPage from '@/features/regist/pages/RegistScanPage';
 import { Route, Routes } from 'react-router-dom';
 import RegistSearchPage from '@/features/regist/pages/RegistSearchPage';
 import RegistCameraPage from '@/features/regist/pages/RegistCameraPage';
-import RegistTestPage from '@/features/regist/pages/RegistTestPage';
 const RegistPage = () => (
 	<Routes>
 		<Route path='/' element={<RegistHomePage />} />
 		<Route path='scan' element={<RegistScanPage />} />
 		<Route path='search' element={<RegistSearchPage />} />
 		<Route path='camera' element={<RegistCameraPage />} />
-		<Route path='test' element={<RegistTestPage />} />
 	</Routes>
 );
 
