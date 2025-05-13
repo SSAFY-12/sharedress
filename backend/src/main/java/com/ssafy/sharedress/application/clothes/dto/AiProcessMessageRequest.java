c1a2822bdd0583ee02db26969d3ed74d147eb18e
지윤 <jiyun0218@naver.com>
2025-05-13 10:10:12 +0900

    🛠️ fix: 구매내역 메시지 SQS 전송 시 옷 30개씩 배치 처리

--- backend/src/main/java/com/ssafy/sharedress/application/clothes/dto/AiProcessMessageRequest.java
+++ backend/src/main/java/com/ssafy/sharedress/application/clothes/dto/AiProcessMessageRequest.java
@@ -3,6 +3,8 @@ package com.ssafy.sharedress.application.clothes.dto;
 import java.util.List;
 
 public record AiProcessMessageRequest(
+	String taskId,
+	Boolean isLast,
 	Long memberId,
 	String fcmToken,
 	List<ItemInfo> items
