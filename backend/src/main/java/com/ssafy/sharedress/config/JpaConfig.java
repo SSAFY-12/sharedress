8954a691adb12c3105c395a2ca8ca5a2e961fe79
주노리 <777joonho@kookmin.ac.kr>
2025-05-15 10:30:50 +0900

    🛠️ fix: BaseAuthor 수정

--- backend/src/main/java/com/ssafy/sharedress/config/JpaConfig.java
+++ backend/src/main/java/com/ssafy/sharedress/config/JpaConfig.java
@@ -12,12 +12,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-	// TODO: 현재 테스트 코드, 추후 주석처리된 내용으로 변경해야함.
 	@Bean
 	public AuditorAware<String> auditorAware() {
 		return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
 			.map(Principal::getName)
-			.orElse("test@email.com")
+			.orElse("anonymous")
 			.describeConstable();
 	}
 }
