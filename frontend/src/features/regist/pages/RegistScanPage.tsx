2111fafc7e888d8bcb5f8ac54627dd060a0eb6bc
김현래 <gusto917@naver.com>
2025-05-15 14:34:45 +0900

    feat: 업데이트 예정 처리

--- frontend/src/features/regist/pages/RegistScanPage.tsx
+++ frontend/src/features/regist/pages/RegistScanPage.tsx
@@ -1,27 +1,28 @@
 import PlatFormBlock from '@/features/regist/components/PlatFormBlock';
 
 const RegistScanPage = () => (
-	<div className='flex-1 w-full h-full flex flex-col justify-start items-center py-5 px-4 gap-5'>
+	<div className='flex-1 w-full h-full flex flex-col justify-start items-center py-5 px-2 gap-1'>
 		<PlatFormBlock
 			title='무신사'
 			description='https://www.musinsa.com'
 			image='musinsa.png'
 		/>
-		<PlatFormBlock
-			title='29CM'
-			description='https://www.29cm.co.kr'
-			image='29cm.png'
-		/>
-		<PlatFormBlock
-			title='에이블리'
-			description='https://m.a-bly.com'
-			image='ably.png'
-		/>
-		<PlatFormBlock
-			title='크림'
-			description='https://kream.co.kr'
-			image='kream.png'
-		/>
+		<div className='flex justify-start items-center gap-4 w-full relative py-2 px-2'>
+			<div className='absolute inset-0 bg-black/30 rounded-lg z-10 flex items-center justify-center'>
+				<span className='text-white text-button w-full pr-4'>
+					업데이트 예정
+				</span>
+			</div>
+			<img
+				src='/images/regist/29cm.png'
+				alt='29cm'
+				className='h-14 object-cover border border-light rounded-lg'
+			/>
+			<div className='flex flex-col items-start gap-1 py-2'>
+				<h1 className='text-smallButton text-regular'>29cm</h1>
+				<p className='text-description'>https://www.29cm.co.kr</p>
+			</div>
+		</div>
 	</div>
 );
 
