6d6f119481176345bf7c5408bae6302965db0a5e
김현래 <gusto917@naver.com>
2025-05-16 17:10:34 +0900

    ✨ feat: 삭제성공시 옷장으로 바로 refresh

--- frontend/src/features/closet/hooks/useDeleteCoordinations.ts
+++ frontend/src/features/closet/hooks/useDeleteCoordinations.ts
@@ -1,7 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCoordination } from '@/features/closet/api/closetApi';
+import { useClosetStore } from '@/store/useClosetStore';
 
-export const useDeleteCoordination = () =>
-	useMutation({
+export const useDeleteCoordination = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
 		mutationFn: deleteCoordination,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['closet'] });
+		},
 	});
+};
