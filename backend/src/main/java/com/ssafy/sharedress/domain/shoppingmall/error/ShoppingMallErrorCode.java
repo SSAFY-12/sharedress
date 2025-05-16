fd0596e0dd4790e169d73c309af68afec66b0756
주노리 <777joonho@kookmin.ac.kr>
2025-05-16 15:05:53 +0900

    🛠️ fix: response status code 동일하게 맞추기

--- backend/src/main/java/com/ssafy/sharedress/domain/shoppingmall/error/ShoppingMallErrorCode.java
+++ backend/src/main/java/com/ssafy/sharedress/domain/shoppingmall/error/ShoppingMallErrorCode.java
@@ -6,7 +6,7 @@ import com.ssafy.sharedress.global.response.ResponseCode;
 
 public enum ShoppingMallErrorCode implements ResponseCode {
 	SHOPPING_MALL_NOT_FOUND("404", "쇼핑몰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-	SHOPPING_MALL_ID_PW_NOT_MATCH("409", "아이디와 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+	SHOPPING_MALL_ID_PW_NOT_MATCH("409", "아이디와 비밀번호가 일치하지 않습니다.", HttpStatus.CONFLICT);
 
 	private final String code;
 	private final String message;
