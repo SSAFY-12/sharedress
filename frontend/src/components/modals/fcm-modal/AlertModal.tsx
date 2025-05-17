e64af2f9b578dbf923f965289f8721bc2a587947
김현래 <gusto917@naver.com>
2025-05-17 15:23:49 +0900

    style: 알림모달 이뻐짐

--- frontend/src/components/modals/fcm-modal/AlertModal.tsx
+++ frontend/src/components/modals/fcm-modal/AlertModal.tsx
@@ -1,4 +1,3 @@
-import { Bell } from 'lucide-react';
 import { MainModal } from '@/components/modals/main-modal/MainModal';
 
 interface AlertModalProps {
@@ -18,28 +17,31 @@ export const AlertModal = ({
 	onConfirm,
 	onHide,
 	title = '알림 활성화 안내',
-	description = '알림을 받으시려면 설정 페이지에서 알림을 활성화해 주세요.',
+	description = `알림을 받기 위해
+설정 페이지에서 활성화해 주세요`,
 	confirmText = '설정으로 이동',
 	hideText = '다신 안보기',
 }: AlertModalProps) => (
 	<MainModal isOpen={isOpen} onClose={onClose}>
 		<MainModal.Header>
 			<div className='flex flex-col items-center gap-2 pb-2'>
-				<Bell className='w-10 h-10 text-black' />
-				<h3 className='text-lg font-bold'>{title}</h3>
+				<img src='/icons/toast_bell.png' alt='fcm' className='w-12 h-12 my-2' />
+				<span className='text-regular text-title'>{title}</span>
 			</div>
 		</MainModal.Header>
 		<MainModal.Body>
-			<p className='text-center text-gray-700 mb-4'>{description}</p>
+			<p className='text-low text-default whitespace-pre-line mb-5'>
+				{description}
+			</p>
 			<div className='flex gap-2'>
 				<button
-					className='flex-1 py-2 bg-black text-white rounded-md font-semibold hover:bg-gray-800 transition'
+					className='flex-1 w-full px-4 py-3 text-smallButton text-regualr bg-brownButton hover:bg-brownButton/80 text-white'
 					onClick={onConfirm}
 				>
 					{confirmText}
 				</button>
 				<button
-					className='flex-1 py-2 bg-gray-200 text-gray-800 rounded-md font-semibold hover:bg-gray-300 transition'
+					className='flex-1 w-full px-4 py-3 text-smallButton bg-background hover:bg-light text-regular rounded-md font-semibold transition'
 					onClick={onHide}
 				>
 					{hideText}
