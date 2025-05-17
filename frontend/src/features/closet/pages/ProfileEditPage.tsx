780a7925ef59ac65c70722f58bc340b2a471fbbb
Yeseung-Park <yspark8538@naver.com>
2025-05-17 23:55:17 +0900

    ✨ feat: 프로필 수정 페이지에서도 글자수 경고 및 확인 추가

--- frontend/src/features/closet/pages/ProfileEditPage.tsx
+++ frontend/src/features/closet/pages/ProfileEditPage.tsx
@@ -8,6 +8,7 @@ import { useForm, Controller } from 'react-hook-form';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { Info } from 'lucide-react';
 interface ProfileEditForm {
 	nickname: string;
 	oneLiner: string;
@@ -174,6 +175,19 @@ const ProfileEditPage = () => {
 								}
 							}}
 						/>
+						<div className='flex justify-between items-center w-full text-xs'>
+							{watch('nickname').length === 10 ? (
+								<span className='flex gap-1.5 items-center text-delete text-smallDescription'>
+									<Info size={14} />
+									10자까지 입력 가능합니다
+								</span>
+							) : (
+								<span />
+							)}
+							<span className='text-descriptionColor'>
+								{watch('nickname').length} / 10
+							</span>
+						</div>
 					</div>
 					<div className='flex flex-col gap-2'>
 						<span className='text-low text-description text-left'>소개</span>
@@ -187,6 +201,19 @@ const ProfileEditPage = () => {
 								}
 							}}
 						/>
+						<div className='flex justify-between items-center w-full text-xs mb-1'>
+							{watch('oneLiner').length === 20 ? (
+								<span className='flex items-center text-red-500'>
+									<Info className='w-3.5 h-3.5 mr-1 shrink-0' />
+									20자까지 입력 가능합니다
+								</span>
+							) : (
+								<span />
+							)}
+							<span className='text-descriptionColor'>
+								{watch('oneLiner').length} / 20
+							</span>
+						</div>
 					</div>
 					<div className='flex justify-between items-center w-full p-2.5'>
 						<span className='text-regular text-default'>프로필 공개</span>
