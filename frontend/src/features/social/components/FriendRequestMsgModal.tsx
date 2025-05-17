c36398838a1cb9cbf8dcaefb2aebb1dcccc154a4
Yeseung-Park <yspark8538@naver.com>
2025-05-17 23:46:10 +0900

    ✨ feat: 친구 요청 시 메시지 입력 제한 글자 수 표시 및 넘칠 경우 경고 메시지 표시

--- frontend/src/features/social/components/FriendRequestMsgModal.tsx
+++ frontend/src/features/social/components/FriendRequestMsgModal.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { MainModal } from '@/components/modals/main-modal/MainModal';
 import { UserMiniAvatar } from '@/components/cards/user-mini-avatar/UserMiniAvatar';
 import { PrimaryBtn } from '@/components/buttons/primary-button';
 import { getOptimizedImageUrl } from '@/utils/imageUtils';
+import { Info } from 'lucide-react';
 
 interface FriendRequestMsgModalProps {
 	// 친구 요청 메시지 모달 컴포넌트의 타입 정의
@@ -25,45 +27,65 @@ export const FriendRequestMsgModal = ({
 	message,
 	onMessageChange,
 	onConfirm,
-}: FriendRequestMsgModalProps) => (
+}: FriendRequestMsgModalProps) => {
 	// console.log('friendRequestMsgModal 랜더링 :', friend); // memberId 값만 로깅
-	<MainModal
-		isOpen={isOpen}
-		onClose={onClose}
-		overlayClassName='bg-black/80 backdrop-blur-sm' // 모달 배경 투명도 조절
-	>
-		<MainModal.Header>
-			<MainModal.Body>
-				<div className='flex flex-col items-center mt-4'>
-					<UserMiniAvatar
-						src={getOptimizedImageUrl(friend.profileImage)}
-						size='lg'
-						className='mb-2.5'
-					/>
-					<h2 className='text-smallButton text-regular mb-6'>
-						{friend.nickname}
-					</h2>
-					<label className='w-full mb-2.5 text-regular text-description text-left'>
-						친구 요청 메시지(10자 이하)
-					</label>
-					<input
-						className='mb-6 text-default text-regular placeholder:text-descriptionColor text-left flex justify-between items-center px-4 py-2.5 bg-background h-12 rounded-md w-full focus:outline-none focus:ring-1 focus:ring-light disabled:bg-background'
-						placeholder='메시지 입력'
-						value={message}
-						onChange={(e) => {
-							if (e.target.value.length <= 10) {
-								onMessageChange(e.target.value);
-							}
-						}}
-					/>
-					<PrimaryBtn
-						size='medium'
-						name='친구 요청'
-						color='primary'
-						onClick={onConfirm}
-					/>
-				</div>
-			</MainModal.Body>
-		</MainModal.Header>
-	</MainModal>
-);
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		if (value.length <= 30) {
+			onMessageChange(value);
+		}
+	};
+	return (
+		<MainModal
+			isOpen={isOpen}
+			onClose={onClose}
+			overlayClassName='bg-black/80 backdrop-blur-sm' // 모달 배경 투명도 조절
+		>
+			<MainModal.Header>
+				<MainModal.Body>
+					<div className='flex flex-col items-center mt-4'>
+						<UserMiniAvatar
+							src={getOptimizedImageUrl(friend.profileImage)}
+							size='lg'
+							className='mb-2.5'
+						/>
+						<h2 className='text-smallButton text-regular mb-6'>
+							{friend.nickname}
+						</h2>
+						<label className='w-full mb-2.5 text-regular text-description text-left'>
+							친구 요청 메시지(30자 이하)
+						</label>
+						<input
+							className='text-default text-regular placeholder:text-descriptionColor text-left flex justify-between items-center px-4 py-2.5 bg-background h-12 rounded-md w-full focus:outline-none focus:ring-1 focus:ring-light disabled:bg-background'
+							placeholder='메시지 입력'
+							value={message}
+							onChange={handleInputChange}
+						/>
+
+						<div className='flex justify-between items-center w-full text-xs mb-6 mt-2'>
+							{message.length === 30 ? (
+								<span className='flex gap-1.5 items-center text-delete text-smallDescription'>
+									<Info size={14} />
+									30자까지 입력 가능합니다
+								</span>
+							) : (
+								<span />
+							)}
+
+							<span className='text-descriptionColor text-smallDescription'>
+								{message.length} / 30
+							</span>
+						</div>
+
+						<PrimaryBtn
+							size='medium'
+							name='친구 요청'
+							color='primary'
+							onClick={onConfirm}
+						/>
+					</div>
+				</MainModal.Body>
+			</MainModal.Header>
+		</MainModal>
+	);
+};
