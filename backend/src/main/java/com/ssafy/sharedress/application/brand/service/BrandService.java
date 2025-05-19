a7fbf643cfbb35a673c270fc665f3bf7a1239c7f
주노리 <777joonho@kookmin.ac.kr>
2025-05-19 10:11:24 +0900

    🛠️ fix: 브랜드 검색 시 없으면 -1로 return

--- backend/src/main/java/com/ssafy/sharedress/application/brand/service/BrandService.java
+++ backend/src/main/java/com/ssafy/sharedress/application/brand/service/BrandService.java
@@ -5,8 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.sharedress.application.brand.dto.BrandSearchResponse;
 import com.ssafy.sharedress.application.brand.usecase.BrandUseCase;
+import com.ssafy.sharedress.domain.brand.error.BrandErrorCode;
 import com.ssafy.sharedress.domain.brand.repository.BrandRepository;
 import com.ssafy.sharedress.global.dto.CursorPageResult;
+import com.ssafy.sharedress.global.exception.ExceptionUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +25,14 @@ public class BrandService implements BrandUseCase {
 		Long cursor,
 		int size
 	) {
-		return brandRepository.searchBrandsWithCursor(keyword, cursor, size);
+		CursorPageResult<BrandSearchResponse> result = brandRepository.searchBrandsWithCursor(
+			keyword, cursor, size);
+
+		if (result.contents().isEmpty()) {
+			result.contents()
+				.add(BrandSearchResponse.from(brandRepository.findById(-1L).orElseThrow(ExceptionUtil.exceptionSupplier(
+					BrandErrorCode.BRAND_NOT_FOUND))));
+		}
+		return result;
 	}
 }
