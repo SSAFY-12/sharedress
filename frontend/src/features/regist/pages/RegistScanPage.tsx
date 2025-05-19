6ef69d1be9450fb7e9721a45b22c9654a960956d
Yeseung-Park <yspark8538@naver.com>
2025-05-19 15:07:40 +0900

    ✨ feat: 라우팅 이상한 것 해결

--- frontend/src/features/regist/pages/RegistScanPage.tsx
+++ frontend/src/features/regist/pages/RegistScanPage.tsx
@@ -7,6 +7,7 @@ import {
 	setPrivacyAgreement,
 } from '@/features/regist/api/registApis';
 import PrivacyModal from '@/features/regist/components/PrivacyModal';
+import Header from '@/components/layouts/Header';
 
 const RegistScanPage = () => {
 	const navigate = useNavigate();
@@ -38,51 +39,66 @@ const RegistScanPage = () => {
 		}
 	};
 
-	return (
-		<div className='flex-1 w-full h-full flex flex-col justify-start items-center py-5 px-2 gap-1'>
-			<PlatFormBlock
-				title='무신사'
-				description='https://www.musinsa.com'
-				image='musinsa.png'
-				onClick={() => {
-					if (!isMusinsaScanning) {
-						navigate('/regist/scan/musinsa');
-					}
-				}}
-			>
-				{isMusinsaScanning && (
-					<div className='absolute inset-0 bg-modify/30 rounded-lg z-10 flex items-center justify-center'>
-						<div className='flex items-center justify-center gap-2'>
-							<img src='/icons/loading.svg' className='w-4 h-4' alt='스캔중' />
-							<span className='text-white text-button w-full pr-4'>
-								스캔 중
-							</span>
-						</div>
-					</div>
-				)}
-			</PlatFormBlock>
-			<div className='flex justify-start items-center gap-4 w-full relative py-2 px-2'>
-				<div className='absolute inset-0 bg-black/30 rounded-lg z-10 flex items-center justify-center'>
-					<span className='text-white text-button w-full pr-2'>
-						업데이트 예정
-					</span>
-				</div>
-				<img
-					src='/images/regist/29cm.png'
-					alt='29cm'
-					className='h-14 object-cover border border-light rounded-lg'
-				/>
-				<div className='flex flex-col items-start gap-1 py-2'>
-					<h1 className='text-smallButton text-regular'>29cm</h1>
-					<p className='text-description'>https://www.29cm.co.kr</p>
-				</div>
-			</div>
+	const handleBack = () => {
+		navigate('/regist');
+	};
 
-			<PrivacyModal
-				isOpen={isPrivacyModalOpen}
-				onClose={() => setIsPrivacyModalOpen(false)}
-				onAgree={handleAgree}
+	return (
+		<div className='flex flex-col h-full flex-1'>
+			<Header
+				showBack={true}
+				subtitle='구매내역 스캔'
+				onBackClick={handleBack}
 			/>
+			<div className='flex-1 w-full h-full flex flex-col justify-start items-center py-5 px-2 gap-1'>
+				<PlatFormBlock
+					title='무신사'
+					description='https://www.musinsa.com'
+					image='musinsa.png'
+					onClick={() => {
+						if (!isMusinsaScanning) {
+							navigate('/regist/scan/musinsa');
+						}
+					}}
+				>
+					{isMusinsaScanning && (
+						<div className='absolute inset-0 bg-modify/30 rounded-lg z-10 flex items-center justify-center'>
+							<div className='flex items-center justify-center gap-2'>
+								<img
+									src='/icons/loading.svg'
+									className='w-4 h-4'
+									alt='스캔중'
+								/>
+								<span className='text-white text-button w-full pr-4'>
+									스캔 중
+								</span>
+							</div>
+						</div>
+					)}
+				</PlatFormBlock>
+				<div className='flex justify-start items-center gap-4 w-full relative py-2 px-2'>
+					<div className='absolute inset-0 bg-black/30 rounded-lg z-10 flex items-center justify-center'>
+						<span className='text-white text-button w-full pr-2'>
+							업데이트 예정
+						</span>
+					</div>
+					<img
+						src='/images/regist/29cm.png'
+						alt='29cm'
+						className='h-14 object-cover border border-light rounded-lg'
+					/>
+					<div className='flex flex-col items-start gap-1 py-2'>
+						<h1 className='text-smallButton text-regular'>29cm</h1>
+						<p className='text-description'>https://www.29cm.co.kr</p>
+					</div>
+				</div>
+
+				<PrivacyModal
+					isOpen={isPrivacyModalOpen}
+					onClose={() => setIsPrivacyModalOpen(false)}
+					onAgree={handleAgree}
+				/>
+			</div>
 		</div>
 	);
 };
